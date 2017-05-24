f7a26bdff0a4d7bd582054542479a7992aa84e20
jamessouth <jamesdanielsouth@gmail.com>
2017-05-24 00:03:41 -0500

    uploads

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ $('video').mediaelementplayer({
 
 const $container = $('#mep_0');
 const $video = $('video');
+const $spans = $('.text');
+
+
+$spans.each(x => {
+	$spans[x].dataset.time = $spans[x].dataset.time.replace(/,/g, '.').replace(/(0+\:)\1*0*/g, '').replace(/1:0/g,'6');
+	
+	let timeArray = $spans[x].dataset.time.split(' --> ');
+	
+	let [btime,etime] = [timeArray[0], timeArray[1]];
+	
+	$($spans[x]).attr({'data-beg-time': btime, 'data-end-time': etime});
+	$($spans[x]).removeAttr('data-time');
+	
+	
+	
+});
+
+
+
+
+
 $video.addClass('fscreen');
 $container.addClass('fscreen fscreen-vid');
 
@@ -16,29 +37,40 @@ $(document).on(screen_change_events, () => {
 	
 });
 
-//  /(0+\:)\1*0*/g
 
 
+console.log($spans);
 
 $video.on('timeupdate', (e) => {
 	const time = e.currentTarget.currentTime;
+	$spans.each(x => {
+		let btime = parseFloat($spans[x].dataset.begTime);
+		let etime = parseFloat($spans[x].dataset.endTime);
+		if(btime <= time && time < etime){
+			$spans[x].style.color = 'orange';
+		} else {
+			$spans[x].style.color = 'black';
+		}
+		
+	});
+	
+	
+	
+	
+	
+	
 	console.log(time);
 
 });
 
-let $s = $('.text');
-let $s2 = $('.text-hid');
-// console.log($s);
-console.log($s2[1]);
-$s.each(x => {
-	if ($s[x].textContent.includes('blah')){
-		console.log($s[x+1]);
-	}
-	
-}
 
 
-);
+
+
+
+
+
+
 
 
 
